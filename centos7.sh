d9abdcba32c8c34fa31af5e5aeb42a9f0a95bb60
nguyenlucmmo <80386111+nguyenlucmmo@users.noreply.github.com>
2021-03-10 15:39:19 +0700

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -58,7 +58,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "lumateam/Lumateam@2019/$IP4/$port/$(gen64 $IP6)"
+        echo "kingofvia.com/kingofvia.com/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
